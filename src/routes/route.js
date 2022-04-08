447f0f93991fb179ba1ed5bca4ca33384e6895bd
sainath47 <reddysainth47@gmail.com>
2022-04-08 20:57:50 +0530

    my 1st commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,157 @@
+const { json } = require('body-parser');
 const express = require('express');
 const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+const candidates = [ 'sainath','sagar','neeraj', 'sarada']
+
+router.get('/all-candidates', function (req, res) {
+   
+
+
+res.send(candidates)
+
 });
+router.get('/candidates', function (req, res) {
+
+
+
+   
+});
+router.get('/movies', function (req, res) {
+
+    const arr = [ 'sainath','sagar','neeraj', 'sarada']
+
+   const sagar = arr.values()
+    res.send(sagar)
+
+
+
+   
+});
+
+
+
+router.get('/movies/:indexNumber', function (req, res) {
+
+    const arr = [ 'sainath','sagar','neeraj', 'sarada']
+
+    res.send(arr[req.params.indexNumber])
+
+});
+
+
+router.get('/movies/:indexNumber', function (req, res) {
+
+    const arr = [ 'sainath','sagar','neeraj', 'sarada']
+
+if(req.params.indexNumber>arr.length-1)
+{
+    let a="please enter a valid input"
+    res.send(a)
+}
+
+else{
+let a=arr[req.params.indexNumber]
+
+    res.send(a)
+}
+
+});
+
+
+
+router.get('/films', function (req, res) {
+
+    const sagar = [ 
+       
+        {
+            "id": 1,
+            "name": "The Shining"},
+        {
+            "id": 2,
+            "name": "pushpa"},
+        {
+            "id": 3,
+            "name": "khiladi"},
+        {
+            "id": 4,
+            "name": "boss"}
+        ]
+
+
+res.send(sagar)
+
+
+
+});
+
+
+
+
+router.get('/films/:filmId', function (req, res) {
+
+
+   const sagar = [ 
+       
+    {
+        "id": 1,
+        "name": "The Shining"},
+    {
+        "id": 2,
+        "name": "pushpa"},
+    {
+        "id": 3,
+        "name": "khiladi"},
+    {
+        "id": 4,
+        "name": "boss"}
+    ]
+
+    for (let i =0; i<sagar.length; i++)
+    {
+      if(req.params['filmId']>sagar.length)
+      {let a = 'No movie exists with this id'
+          res.send(a)
+      }
+    
+
+      else if(sagar[i].id == req.params['filmId'])
+      {
+          let a= sagar[i].name
+          res.send(a)
+      }
+  
+  
+    }
+  
+    
+    
+      
+
+  
+
+});
+
+
+
+// router.get('/movies/:indexNumber', function (req, res) {
+
+//     const arr = [ 'sainath','sagar','neeraj', 'sarada']
+
+// if()
+
+//     res.send(arr[req.params.indexNumber])
+
+// });
+//   req.params.indexNumber
+
+
+
+
+ 
+
 
 
 
