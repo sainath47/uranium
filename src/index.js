65939fac6ce9528ebfa8e9cdd7d79530e2a8ab67
sainath47 <reddysainth47@gmail.com>
2022-04-11 14:52:33 +0530

    apis

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
@@ -9,6 +10,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
+
+// mongodb+srv://SAINATH47:COOLESTBEING@cluster0.fk14j.mongodb.net/test
+
+mongoose.connect("mongodb+srv://SAINATH47:COOLESTBEING@cluster0.fk14j.mongodb.net/SAINATH47",{useNewUrlParser=true}).try({
+    console.log("mongodb is connected")
+}).catch(error)
+
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
