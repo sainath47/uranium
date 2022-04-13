336f87f9373a7ca720616d32e655b44ed3640667
sainath47 <reddysainth47@gmail.com>
2022-04-13 06:07:26 +0530

    lo bhai

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,91 +1,71 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ]
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ]
-       },
-   ]
+let players = [
+  {
+    name: "manish",
+    dob: "1/1/1995",
+    gender: "male",
+    city: "jalandhar",
+    sports: ["swimming"],
+  },
+  {
+    name: "gopal",
+    dob: "1/09/1995",
+    gender: "male",
+    city: "delhi",
+    sports: ["soccer"],
+  },
+  {
+    name: "lokesh",
+    dob: "1/1/1990",
+    gender: "male",
+    city: "mumbai",
+    sports: ["soccer"],
+  },
+];
+
+router.post("/players", function (req, res) {
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].name === req.body.name) {
+      return res.send("player name already exits ");
+
+    } 
+
+  }
+    players.push(req.body);
+
+   return res.send(players);
 
 
-
-
-router.post('/players', function (req, res) {
-
-    for(let i=0; i<players.length; i++)
-    {
-if (players[i].name!==req.body.name)
-{
-    players.push(req.body)
-
-    res.send(players)
-}
-
-else{
-    res.send("player name already exits ")
-}
-    }
-
+  
 });
-
-
 
 // router.get('/test-api1', function (req, res) {
 
 //     res.send( "hi FunctionUp " )
 // });
 
-
 // router.get('/test-api2', function (req, res) {
 
 //     res.send( { msg: "Hi FUnctionUp..again !"} )
 // });
-
 
 // router.get('/test-api3', function (req, res) {
 
 //     res.send( { msg: "Hi FUnctionUp..again..this is another similar api !"} )
 // });
 
-
 // router.get('/test-api4', function (req, res) {
 
 //     res.send( { msg: "Hi FUnctionUp..again..this is another similar api ..not I am getting bored!"} )
 // });
 
-
 // router.get('/test-api5', function (req, res) {
 
 //     res.send( { msg: "Hi FUnctionUp" , name:"FunctionUp", age: "100"} )
 // });
-
-
 
 // router.get('/test-api6', function (req, res) {
 
@@ -97,7 +77,6 @@ else{
 //     res.send( {  msg: "hi guys"  }   )
 // });
 
-
 // // to send data in  post request-> prefer sending in BODY -> click body-raw-json
 // router.post('/test-post2', function (req, res) {
 //     let data= req.body
@@ -105,11 +84,8 @@ else{
 //     res.send( {  msg: "hi guys..my 2nd post req"  }   )
 // });
 
-
 // const randomController= require("../controllers/randomController.js")
 // //write a post request to accept an element in post request body and add it to the given array and return the new array
 // router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
-
-
 
 module.exports = router;
